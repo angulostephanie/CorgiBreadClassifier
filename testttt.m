e0c1f0dd1845b85c97f0a65ef745b644d0af1101
angulostephanie <angulostephanie21@gmail.com>
2017-12-01 19:04:41 -0800

    corrupt files still exist

--- testttt.m
+++ testttt.m
@@ -2,12 +2,13 @@ disp('Hi');
 rootFolder = '/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/machine-learning/final_project/data';
 categories = {'corgi_data','bread_data'};
 imgs = imageDatastore(fullfile(rootFolder, categories),'LabelSource', ...
-    'foldernames', 'IncludeSubfolders', true, 'FileExtensions', '.jpg');
-imgs.ReadFcn = @(filename)readAndPreprocessImage(filename);
+    'foldernames', 'IncludeSubfolders', true, 'FileExtensions', '.jpg', ...
+    'ReadFcn', @(filename)readAndPreprocessImage(filename));
+% imgs.ReadFcn = @(filename)readAndPreprocessImage(filename);
 [train, test] = splitEachLabel(imgs, 50, 'randomize');
 
-options = trainingOptions('sgdm', 'MaxEpochs', 20, 'InitialLearnRate', ...
-    .001, 'plots', 'training-progress');
+options = trainingOptions('sgdm', 'MaxEpochs', 3, 'InitialLearnRate', ...
+    .001);
 
 layers = [imageInputLayer([28 28 3])
           fullyConnectedLayer(2)
@@ -19,14 +20,21 @@ test_output = classify(net, test);
 accuracy = sum(test_output == testing.Labels)/length(test_output); 
 
 function Iout = readAndPreprocessImage(filename)
-I = imread(filename);
-% Some images may be grayscale. Replicate the image 3 times to
+
+try
+    I = imread(filename);
+    % Some images may be grayscale. Replicate the image 3 times to
 % create an RGB image.
-if ismatrix(I)
-    I = cat(3,I,I,I);
-end
+    if ismatrix(I)
+        I = cat(3,I,I,I);
+    end
 % Resize the image as required for the CNN.
-Iout = imresize(I, [28 28]);
+    Iout = imresize(I, [28 28]);
 %imwrite(Iout, filename);
+catch
+    disp('nah');
+    disp(filename);
+    Iout = null;
 end
-% images.ReadFcn = @(filename)readAndPreprocessImage(filename);
+
+end
