0d6d1b765e7080d812bb7cfd8ad31dd995d4e7fc
angulostephanie <angulostephanie21@gmail.com>
2017-12-05 21:53:41 -0800

    deleted irrelevant data

--- Constants.m
+++ Constants.m
@@ -2,7 +2,7 @@ classdef Constants
     properties (Constant)
         STEPHS_DIRECTORY = '/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/machine-learning/final_project/data';
         EMILYS_DIRECTORY = 'D:\CodeStuff\MATLAB\Machine_Learning\DoggoProject\data';
-        TRAINING_SIZE = 100;
+        TRAINING_SIZE = 700;
         IMG_SIZE = 100;
         % will add other constants throughout the project
     end
--- CorgiBreadClassifier.m
+++ CorgiBreadClassifier.m
@@ -1,34 +1,34 @@
-% root_folder = Constants.STEPHS_DIRECTORY; 
-% categories = {'corgi_data','bread_data'};
-% imgs = imageDatastore(fullfile(root_folder, categories),'LabelSource', ...
-%     'foldernames', 'IncludeSubfolders', true, 'FileExtensions', '.jpg');
-% imgs.ReadFcn = @(loc)imresize(imread(loc),[Constants.IMG_SIZE, Constants.IMG_SIZE]); % resize images
-% N = numel(imgs.Files);
-% imageSize = nan(N,2);
-% for i = 1:N
-%    try
-%        img = readimage(imgs,i);
-% %        fprintf('File #: %d\nSize %d-by-%d-by-%d\n', i, size(img,1), size(img,2), size(img,3));
-%        if(size(img,3) < 3)
-%             delete(imgs.Files{i}); % delete non rbg photos
-%             disp(i);
-%        end
-%    catch
-%         delete(imgs.Files{i}); % delete corrupt images
-%         disp(i);
-%    end
-% end
-% 
-% [train, test] = splitEachLabel(imgs, Constants.TRAINING_SIZE, 'randomize'); 
-% options = trainingOptions('sgdm', 'MaxEpochs', 3, 'InitialLearnRate', ...
-%     .001);
-% layers = [imageInputLayer([Constants.IMG_SIZE Constants.IMG_SIZE 3])
-%           fullyConnectedLayer(2)
-%           softmaxLayer
-%           classificationLayer];
-% net = trainNetwork(train, layers, options);
-% test_output = classify(net, test);
-% accuracy = sum(test_output == test.Labels)/length(test_output); 
+root_folder = Constants.STEPHS_DIRECTORY; 
+categories = {'corgi_data','bread_data'};
+imgs = imageDatastore(fullfile(root_folder, categories),'LabelSource', ...
+    'foldernames', 'IncludeSubfolders', true, 'FileExtensions', '.jpg');
+imgs.ReadFcn = @(loc)imresize(imread(loc),[Constants.IMG_SIZE, Constants.IMG_SIZE]); % resize images
+N = numel(imgs.Files);
+imageSize = nan(N,2);
+for i = 1:N
+   try
+       img = readimage(imgs,i);
+%        fprintf('File #: %d\nSize %d-by-%d-by-%d\n', i, size(img,1), size(img,2), size(img,3));
+       if(size(img,3) < 3)
+            delete(imgs.Files{i}); % delete non rbg photos
+            disp(i);
+       end
+   catch
+        delete(imgs.Files{i}); % delete corrupt images
+        disp(i);
+   end
+end
+
+[train, test] = splitEachLabel(imgs, Constants.TRAINING_SIZE, 'randomize'); 
+options = trainingOptions('sgdm', 'MaxEpochs', 5, 'InitialLearnRate', ...
+    .001);
+layers = [imageInputLayer([Constants.IMG_SIZE Constants.IMG_SIZE 3])
+          fullyConnectedLayer(2)
+          softmaxLayer
+          classificationLayer];
+net = trainNetwork(train, layers, options);
+test_output = classify(net, test);
+accuracy = sum(test_output == test.Labels)/length(test_output); 
 
 m = numel(test.Files);
 predictedT = table(test_output);
@@ -37,7 +37,7 @@ predicted = table2cell(predictedT);
 actual = table2cell(actualT);
 
 counter = 1;
-for i = 1:(m/10)
+for i = 1:m
     if(predicted{i} ~= actual{i})
         
         disp(test.Files{i});
