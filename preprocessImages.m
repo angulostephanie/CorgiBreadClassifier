60b1cacc9a0a6b402dd8ebae539a1dd811fbe446
angulostephanie <angulostephanie21@gmail.com>
2017-12-11 13:33:47 -0800

    reformatting/added descriptions to functions

--- preprocessImages.m
+++ preprocessImages.m
@@ -1,20 +1,23 @@
 function [better_imgs] = preprocessImages(imgs)
-%Summary of this function goes here
-imgs.ReadFcn = @(loc)imresize(imread(loc),[Constants.IMG_SIZE, Constants.IMG_SIZE]); % resize images
+% This function resizes images based on the IMG_SIZE constant and deletes
+% corrupt and nonrbg images.
+
+imgs.ReadFcn = @(loc)imresize(imread(loc),[Constants.IMG_SIZE, Constants.IMG_SIZE]); 
 N = numel(imgs.Files);
+
 for i = 1:N
    try
        img = readimage(imgs,i);
-%        fprintf('File #: %d\nSize %d-by-%d-by-%d\n', i, size(img,1), size(img,2), size(img,3));
        if(size(img,3) < 3)
-            delete(imgs.Files{i}); % delete non rbg photos
+            delete(imgs.Files{i}); 
             disp(i);
        end
    catch
-        delete(imgs.Files{i}); % delete corrupt images
+        delete(imgs.Files{i});
         disp(i);
    end
 end
+
 better_imgs = imgs;
 end
 
